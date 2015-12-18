6647353658d905c4105f58fc7854fc434b772328
Devxhkl <devxhkl@outlook.com>
2015-12-18 13:45:07 +0100

    Figuring out what layout to have

---
+++ NYE/CityCell.swift
@@ -0,0 +1,14 @@
+  //
+//  CityCell.swift
+//  NYE
+//
+//  Created by Zel Marko on 17/12/15.
+//  Copyright © 2015 devxhkl. All rights reserved.
+//
+
+import UIKit
+
+class CityCell: UICollectionViewCell {
+    
+  @IBOutlet weak var coutdownLabel: UILabel!
+}
---
+++ NYE/TimelineViewController.swift
@@ -0,0 +1,56 @@
+//
+//  TimelineViewController.swift
+//  NYE
+//
+//  Created by Zel Marko on 17/12/15.
+//  Copyright © 2015 devxhkl. All rights reserved.
+//
+
+import UIKit
+
+class TimelineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension TimelineViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+  
+  func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+    return 1
+  }
+  
+  func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 1
+  }
+  
+  func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CityCell", forIndexPath: indexPath)
+    
+    return cell
+  }
+  
+  func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+    return UIEdgeInsetsMake(50, 50, 50, 50)
+  }
+}
