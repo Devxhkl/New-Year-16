d72b376d2e9baee7cbda80977c92cc19e1c79143
Devxhkl <devxhkl@outlook.com>
2015-12-19 23:56:54 +0100

    Populated split view with cities

---
+++ NYE/TimelineTableViewController.swift
@@ -0,0 +1,95 @@
+//
+//  TimelineTableViewController.swift
+//  NYE
+//
+//  Created by Zel Marko on 19/12/15.
+//  Copyright © 2015 devxhkl. All rights reserved.
+//
+
+import UIKit
+
+class TimelineTableViewController: UITableViewController {
+  
+  let cities = ["Apia", "Chatham Islands", "Auckland", "Anadyr", "Sydney", "Adelaide", "Brisbane", "Darwin", "Tokyo", "Eucla", "Pyongyang", "Hong Kong", "Bangkok", "Mandalay", "Almaty", "Kathmandu", "Mumbai", "Karachi", "Kabul", "Dubai", "Tehran", "Moscow", "Cairo", "Paris", "London", "Ittoqqortoormiit", "Rio de Janeiro", "Buenos Aires", "St. John’s", "La Paz", "Caracas", "New York", "Mexico City", "Calgary", "Los Angeles", "Anchorage", "French Polynesia", "Honolulu", "Alofi", "Baker Island"]
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Uncomment the following line to preserve selection between presentations
+    // self.clearsSelectionOnViewWillAppear = false
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  // MARK: - Table view data source
+  
+  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    // #warning Incomplete implementation, return the number of sections
+    return 1
+  }
+  
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // #warning Incomplete implementation, return the number of rows
+    return cities.count
+  }
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("CityCell", forIndexPath: indexPath)
+    
+    cell.textLabel?.text = cities[indexPath.row]
+    
+    return cell
+  }
+  
+  /*
+  // Override to support conditional editing of the table view.
+  override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+  // Return false if you do not want the specified item to be editable.
+  return true
+  }
+  */
+  
+  /*
+  // Override to support editing the table view.
+  override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+  if editingStyle == .Delete {
+  // Delete the row from the data source
+  tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+  } else if editingStyle == .Insert {
+  // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+  }
+  }
+  */
+  
+  /*
+  // Override to support rearranging the table view.
+  override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+  
+  }
+  */
+  
+  /*
+  // Override to support conditional rearranging of the table view.
+  override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+  // Return false if you do not want the item to be re-orderable.
+  return true
+  }
+  */
+  
+  /*
+  // MARK: - Navigation
+  
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+  // Get the new view controller using segue.destinationViewController.
+  // Pass the selected object to the new view controller.
+  }
+  */
+  
+}
